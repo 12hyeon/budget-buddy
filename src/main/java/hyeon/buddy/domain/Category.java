82c2c8fafbbff45c3a9bc87d785e385d5c55ee35
12hyeon <hyeonjeong1212@naver.com>
2023-11-13 00:26:57 +0900

    feat: 카테고리 domain 추가

---
+++ src/main/java/hyeon/buddy/domain/Category.java
@@ -0,0 +1,32 @@
+package hyeon.buddy.domain;
+
+import hyeon.buddy.dto.CategoryRequestDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    public static Category from(CategoryRequestDTO dto){
+        return Category.builder()
+                .title(dto.getTitle())
+                .build();
+
+    }
+}
